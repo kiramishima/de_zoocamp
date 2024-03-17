9980ab1ffca744659305522343919aba4d0993db
Kira Mishima <kiramishima@outlook.com>
2024-03-17 14:28:43 -0600

    Week 6 completed

---
+++ week_6/parsing_data_2.py
@@ -0,0 +1,48 @@
+import pyspark
+from pyspark.sql import SparkSession
+from pyspark.sql import types
+from pyspark.sql import functions as F
+
+pyspark_version = pyspark.__version__
+kafka_jar_package = f"org.apache.spark:spark-sql-kafka-0-10_2.12:{pyspark_version}"
+
+spark = SparkSession \
+    .builder \
+    .master("local[*]") \
+    .appName("GreenTripsConsumer") \
+    .config("spark.jars.packages", kafka_jar_package) \
+    .config("spark.sql.streaming.forceDeleteTempCheckpointLocation", "true") \
+    .getOrCreate()
+
+green_stream = spark \
+    .readStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", "localhost:9092") \
+    .option("subscribe", "green-trips") \
+    .option("startingOffsets", "earliest") \
+    .load()
+
+schema = types.StructType() \
+    .add("lpep_pickup_datetime", types.StringType()) \
+    .add("lpep_dropoff_datetime", types.StringType()) \
+    .add("PULocationID", types.IntegerType()) \
+    .add("DOLocationID", types.IntegerType()) \
+    .add("passenger_count", types.DoubleType()) \
+    .add("trip_distance", types.DoubleType()) \
+    .add("tip_amount", types.DoubleType())
+
+green_stream = green_stream \
+  .select(F.from_json(F.col("value").cast('STRING'), schema).alias("data")) \
+  .select("data.*")
+
+# Example take it from databricks
+def debug_and_process(batch_df, batch_id):
+    print(f"Batch ID: {batch_id}")
+    batch_df.show()
+
+query = green_stream \
+    .writeStream \
+    .foreachBatch(debug_and_process) \
+    .start()
+
+query.awaitTermination()
---
+++ week_6/popular_dest.py
@@ -0,0 +1,50 @@
+import pyspark
+from pyspark.sql import SparkSession
+from pyspark.sql import types
+from pyspark.sql import functions as F
+
+pyspark_version = pyspark.__version__
+kafka_jar_package = f"org.apache.spark:spark-sql-kafka-0-10_2.12:{pyspark_version}"
+
+spark = SparkSession \
+    .builder \
+    .master("local[*]") \
+    .appName("GreenTripsConsumer") \
+    .config("spark.jars.packages", kafka_jar_package) \
+    .getOrCreate()
+
+green_stream = spark \
+    .readStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", "localhost:9092") \
+    .option("subscribe", "green-trips") \
+    .option("startingOffsets", "earliest") \
+    .load()
+
+schema = types.StructType() \
+    .add("lpep_pickup_datetime", types.StringType()) \
+    .add("lpep_dropoff_datetime", types.StringType()) \
+    .add("PULocationID", types.IntegerType()) \
+    .add("DOLocationID", types.IntegerType()) \
+    .add("passenger_count", types.DoubleType()) \
+    .add("trip_distance", types.DoubleType()) \
+    .add("tip_amount", types.DoubleType())
+
+
+green_stream = green_stream \
+    .select(F.from_json(F.col("value").cast('STRING'), schema).alias("data")) \
+    .select("data.*")
+
+popular_destinations = green_stream.withColumn("timestamp", F.current_timestamp()) \
+    .groupBy(F.window("timestamp", "5 minutes"), "DOLocationID") \
+    .count() \
+    .orderBy(F.col("count").desc())
+
+query = popular_destinations \
+    .writeStream \
+    .outputMode("complete") \
+    .format("console") \
+    .option("truncate", "false") \
+    .start()
+
+query.awaitTermination()
